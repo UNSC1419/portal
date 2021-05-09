c38a67c8809b33ac4ebef697825e719c0d903328
UNSC1419 <ahtamsadik@gmail.com>
2021-05-09 17:41:12 +0900

    修改$servername_radius和$servername_portal为开发环境

--- link_mysql.php
+++ link_mysql.php
@@ -6,10 +6,10 @@ $servername_portal = "192.168.3.249";
 $username_portal = "portal-test";
 $password_portal = "SSZkY8FLwM6As5ZT";
 //radius数据库参数
-$servername_radius = "192.168.3.246";
+$servername_radius = "192.168.3.249";
 $username_radius = "toughradius-test";
 $password_radius = "DzL4fkAwdpchhH87";
- 
+
 // 创建连接portal数据库
 $conn_portal = new mysqli($servername_portal, $username_portal, $password_portal);
 // 检测连接portal数据库
@@ -17,7 +17,7 @@ if ($conn_portal->connect_error) {
     die("连接portal失败: " . $conn_portal->connect_error);
 }
 //未声明情况下$conn_portal所有操作均为portal数据库
-mysqli_select_db($conn_portal,"portal-test");
+mysqli_select_db($conn_portal, "portal-test");
 // 创建连接toughradius数据库
 $conn_radius = new mysqli($servername_radius, $username_radius, $password_radius);
 // 检测连接toughradius数据库
@@ -25,6 +25,6 @@ if ($conn_radius->connect_error) {
     die("连接radius失败: " . $conn_radius->connect_error);
 }
 //未声明情况下$conn_radius所有操作均为toughradius数据
-mysqli_select_db($conn_radius,"toughradius-test");
+mysqli_select_db($conn_radius, "toughradius-test");
 
 ?>
